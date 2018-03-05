ccdbd972cd60d2eca7edb327417f1d54be238ca9
k-kiuchi <minismahn@yahoo.co.jp>
2018-03-05 15:30:08 +0900

    bug fix

--- app/src/main/java/jp/ne/keisuke/kiuchi/circlecisample/MainActivity.kt
+++ app/src/main/java/jp/ne/keisuke/kiuchi/circlecisample/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.ne.keisuke.kiuchi.circlecisample
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
