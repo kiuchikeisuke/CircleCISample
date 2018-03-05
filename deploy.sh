2260558bc78653b3536d339861226d366918a296
k-kiuchi <minismahn@yahoo.co.jp>
2018-03-05 16:16:43 +0900

    fix config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
+
 curl -F "file=@app/build/outputs/apk/app-debug.apk" \
-     -F "token=19394beeb1076eaac059cb948970e59d7bff946d " \
-     -F "message=$CIRCLE_BUILD_NUM" \
+     -F "token=${DEPLOY_GATE_API_KEY}" \
+     -F "message=${CIRCLE_BUILD_NUM}" \
      https://deploygate.com/api/users/kiuchikeisuke/apps
